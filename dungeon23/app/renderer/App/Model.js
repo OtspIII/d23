54b2ff9f1b6e5a47db3be7e7994cb8f5bc07db32
Misha Favorov <otspiii@gmail.com>
2022-12-27 14:01:07 -0500

    Results can have fancy effects, like rolling bonus results

--- dungeon23/app/renderer/App/Model.js
+++ dungeon23/app/renderer/App/Model.js
@@ -1,6 +1,7 @@
 import God from "./God.js";
 import React from 'react';
 import fs from 'fs';
+import { table } from "console";
 
 var Model = {
   Path: `${__dirname}\\modules\\`.replace('renderer\\App', 'renderer').replace('\\app.asar', ''),
@@ -25,15 +26,16 @@ var Model = {
           n++;
           continue;
         }
+        // if(entries[2] ) console.log("."+entries[2]+"." + entries[2].length);
         let line = {
-          Weight: entries[0] ? entries[0] : 1,
+          Weight: entries[0] && entries[0] != "" ? entries[0] : 1,
           Text: entries[1],
-          Info: entries[2],
+          Info: entries[2] && entries[2].length > 1 ? JSON.parse(entries[2]) : null,
         };
         if(line.Text || line.Info)
           table.Entries.push(line);
       }
-      console.log(table);
+      // console.log(table);
       Model.TDict[table.Name] = table;
     }
     // console.log(tables);
@@ -41,9 +43,18 @@ var Model = {
   SplitLine(line){
     let r = [];
     let inQuotes = false;
+    let json = false;
     let substr = "";
     for(let n = 0;n < line.length;n++){
-      if(line[n] == "\"") {
+      if(line[n] == "{") {
+        substr += line[n];
+        json = true;
+      }
+      else if(line[n] == "}") {
+        substr += line[n];
+        json = false;
+      }
+      else if(line[n] == "\"" && !json) {
         if(inQuotes){
           inQuotes = false;
         }
@@ -51,7 +62,7 @@ var Model = {
           inQuotes = true;
         console.log(n + " / " + line);
       }
-      else if(line[n] == "," && !inQuotes){
+      else if(line[n] == "," && !inQuotes && !json){
         r.push(substr);
         substr = "";
       }
@@ -61,13 +72,6 @@ var Model = {
     r.push(substr);
     return r;
   },
-  SetThing(thing,force) {
-    if (!thing){
-      return false;
-    }
-    
-    return true;
-  },
   Get(tag){
     
     if (!this.TDict[tag] )
@@ -76,23 +80,46 @@ var Model = {
   },
   RollOnTable(table){
     if(!table || !table.Entries || table.Entries.length == 0) return;
-    let totalW = 0;
-    for(let ent of table.Entries) totalW += Model.GetWeight(ent);
-    let roll = Math.random() * totalW;
-    for(let ent of table.Entries){
-      roll -= Model.GetWeight(ent);
-      if(roll <= 0) {
-        let result = {Table:table.Name,Readout:[], ID:Math.random()};
-        result.Readout.push(ent.Text);
-        God.Results.AddResult(result);
-        return;
-      }
-    }
-    God.Results.AddResult("ERROR ERROR");
+    let result = {Table:table.Name,Readout:[], ID:Math.random()};
+    let picked = Model.PickRandom(table);
+    let depth = 0;
+    Model.HandleEntry(picked,result,table,depth);
+    God.Results.AddResult(result);
   },
   GetWeight(entry){
     let w = parseInt(entry.Weight) ? parseInt(entry.Weight) : 1;
     return w;
+  },
+  PickRandom(table){
+    let totalW = 0;
+    for(let ent of table.Entries) totalW += Model.GetWeight(ent);
+    let roll = Math.random() * totalW;
+    let rollTxt = Math.ceil(roll);
+    for(let ent of table.Entries){
+      roll -= Model.GetWeight(ent);
+      if(roll <= 0) return {Entry:ent,Roll:rollTxt};
+    }
+    console.log("FAILED TO PICK FROM TABLE: " + table.Name);
+  },
+  HandleEntry(picked,result,table,depth){
+    depth++;
+    if(depth > 99) return;
+    let ent = picked.Entry;
+    result.Readout.push({Text:ent.Text,Roll:picked.Roll,Depth:depth});
+    let info = ent.Info;
+    if(info){
+      if(info.RollOn){
+        let nextTable = Model.Get(info.RollOn.Table);
+        if(!nextTable) nextTable = table;
+        let rolls = info.RollOn.Amount ? info.RollOn.Amount : 1;
+        for(let n = 0;n < rolls;n++){
+          let nPicked = Model.PickRandom(nextTable);
+          Model.HandleEntry(nPicked,result,nextTable,depth);
+        } 
+      }
+      // console.log("INFO: " + ent.Info);
+      // result.Readout.push({Text:JSON.stringify(ent.Info)});
+    }
   }
 }
 
--- dungeon23/app/renderer/App/RollResult.js
+++ dungeon23/app/renderer/App/RollResult.js
@@ -14,8 +14,14 @@ class RollResult extends React.Component {
     let cl = this.props.first ? "Result First" : "Result";
     let res = this.props.result;
     let readouts = [];
-    for(let read of res.Readout)
-      readouts.push(<div key={"Read"+readouts.length}>{read}</div>);
+    for(let read of res.Readout){
+      let roll = read.Roll ? read.Roll + ": " : "";
+      let rcl = "ResultReadout";
+      if(read.Class) rcl += " " + read.Class;
+      let depth = Math.min(read.Depth,4);
+      rcl += " Depth"+depth;
+      readouts.push(<div key={"Read"+readouts.length} className={rcl}>{roll}{read.Text}</div>);
+    }
 
     return (<div className={cl}>{res.Table}
       {readouts}
